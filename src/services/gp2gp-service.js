5ef7ef4771597883b5c5f4439177abf4aabd60f0
Rosie Amphlett <rosie.amphlett@thoughtworks.com>
2020-03-03 17:05:42 +0000

    [PRMT-989] Refactored gp2gp url

--- src/services/gp2gp-service.js
+++ src/services/gp2gp-service.js
@@ -4,7 +4,7 @@ import * as config from '../config/index';
 
 export const sendRequest = nhsNumber => {
   const { gp2gpUrl, gp2gpAuthKeys } = config.default;
-  const url = `${gp2gpUrl}/${nhsNumber}`;
+  const url = `${gp2gpUrl}/pds-retrieval/${nhsNumber}`;
   return new Promise((resolve, reject) => {
     return axios
       .get(url, { headers: { Authorization: gp2gpAuthKeys } })

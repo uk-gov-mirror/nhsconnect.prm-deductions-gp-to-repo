7ddc4b007063bb762a4d3e4fbe94a03efac25529
Rosie Amphlett <rosie.amphlett@thoughtworks.com>
2020-03-05 14:04:49 +0000

    [PRMT-989] Added tests for gp2gp service

--- src/services/__tests__/gp2gp-service.test.js
+++ src/services/__tests__/gp2gp-service.test.js
@@ -8,31 +8,31 @@ jest.mock('../../middleware/logging');
 jest.mock('axios');
 
 const axiosHeaders = {
-    headers: {
-        Authorization: config.gp2gpAuthKeys
-    }
+  headers: {
+    Authorization: config.gp2gpAuthKeys
+  }
 };
 
 describe('gp2gp-adaptor', () => {
-    it('should call axios with nhs number by default and return 200', () => {
-        axios.get.mockResolvedValue({ status: 200 });
-        const nhsNumber = '0123456789';
+  it('should call axios with nhs number by default and return 200', () => {
+    axios.get.mockResolvedValue({ status: 200 });
+    const nhsNumber = '0123456789';
 
-        return sendRequest(nhsNumber).then(response => {
-            expect(response.status).toBe(200);
-            expect(axios.get).toBeCalledWith(
-                `${config.gp2gpUrl}/pds-retrieval/${nhsNumber}`,
-                axiosHeaders
-            );
-        });
+    return sendRequest(nhsNumber).then(response => {
+      expect(response.status).toBe(200);
+      expect(axios.get).toBeCalledWith(
+        `${config.gp2gpUrl}/pds-retrieval/${nhsNumber}`,
+        axiosHeaders
+      );
     });
+  });
 
-    it('should call updateLogEventWithError if there is an error with axios.post request', () => {
-        axios.get.mockRejectedValue(new Error());
-        const nhsNumber = '0123456789';
+  it('should call updateLogEventWithError if there is an error with axios.post request', () => {
+    axios.get.mockRejectedValue(new Error());
+    const nhsNumber = '0123456789';
 
-        return expect(sendRequest(nhsNumber)).rejects.toThrowError(
-            `POST ${config.gp2gpUrl}/pds-retrieval/${nhsNumber} - Request failed`
-        );
-    });
+    return expect(sendRequest(nhsNumber)).rejects.toThrowError(
+      `POST ${config.gp2gpUrl}/pds-retrieval/${nhsNumber} - Request failed`
+    );
+  });
 });

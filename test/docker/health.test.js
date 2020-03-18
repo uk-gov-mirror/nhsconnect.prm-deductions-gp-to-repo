722b0f6e266d68b1480763c36bc34f17432f6558
Rosie Amphlett <rosie.amphlett@thoughtworks.com>
2020-03-18 16:02:46 +0000

    [PRMT-1006] Refactored tests so they are consistent with GP2GP adaptor

---
+++ test/docker/health.test.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import adapter from 'axios/lib/adapters/http';
+import config from '../../src/config';
+
+describe('/health', () => {
+    const baseURL = (process.env.GPTOREPO_URL) ? process.env.GPTOREPO_URL : config.url;
+    const healthUrl = `${baseURL}/health`;
+    it('should return 200', () => {
+        return expect(
+            axios.get(`${healthUrl}`, {
+                adapter
+            })
+        ).resolves.toEqual(expect.objectContaining({ status: 200 }));
+    });
+
+    it('health endpoint returns matching data', async () => {
+        return expect(
+            axios.get(`${healthUrl}`, {
+                adapter
+            })
+        ).resolves.toEqual(
+            expect.objectContaining({
+                data: expect.objectContaining({
+                    version: '1',
+                    description: 'Health of Deductions GP to Repo Component'
+                })
+            })
+        );
+    });
+});

56a9984c18ae6dcb9b6d26605cf572263b8f8183
Rosie Amphlett <rosie.amphlett@thoughtworks.com>
2020-03-18 17:28:12 +0000

    [PRMT-1006] Updated test_docker to set auth keys

--- test/functional/deduction-requests.test.js
+++ test/functional/deduction-requests.test.js
@@ -5,9 +5,7 @@ import config from '../../src/config';
 describe('End to end test of /deduction-requests/:nhsNumber', () => {
     it('should return a 204 from GP2GP Adaptor with a valid NHS number', () => {
         const nhsNumber =
-            process.env.NHS_ENVIRONMENT === 'dev'
-                ? "9473480032"
-                : "9442964410";
+            process.env.NHS_ENVIRONMENT === 'dev' ? "9473480032" : "9442964410";
 
         return expect(
             axios.post(`${config.url}/deduction-requests/${nhsNumber}`, {

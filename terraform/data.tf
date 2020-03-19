1a9b5269b08f3c7736e176015c21bfbaf0cbd35e
Phil Evans <phil.evans@thoughtworks.com>
2020-03-19 22:45:59 +0000

    Removing public ALB in deductions private

--- terraform/data.tf
+++ terraform/data.tf
@@ -32,25 +32,25 @@ data "aws_ssm_parameter" "deductions_private_private_subnets" {
   name = "/nhs/${var.environment}/deductions_private_private_subnets"
 }
 
-data "aws_ssm_parameter" "deductions_private_alb_dns" {
-  name = "/nhs/${var.environment}/deductions_private_alb_dns"
-}
+# data "aws_ssm_parameter" "deductions_private_alb_dns" {
+#   name = "/nhs/${var.environment}/deductions_private_alb_dns"
+# }
 
 data "aws_ssm_parameter" "deductions_private_vpc_id" {
   name = "/nhs/${var.environment}/deductions_private_vpc_id"
 }
 
-data "aws_ssm_parameter" "deductions_private_alb_arn" {
-  name = "/nhs/${var.environment}/deductions_private_alb_arn"
-}
+# data "aws_ssm_parameter" "deductions_private_alb_arn" {
+#   name = "/nhs/${var.environment}/deductions_private_alb_arn"
+# }
 
-data "aws_ssm_parameter" "deductions_private_alb_httpl_arn" {
-  name = "/nhs/${var.environment}/deductions_private_alb_httpl_arn"
-}
+# data "aws_ssm_parameter" "deductions_private_alb_httpl_arn" {
+#   name = "/nhs/${var.environment}/deductions_private_alb_httpl_arn"
+# }
 
-data "aws_ssm_parameter" "deductions_private_alb_httpsl_arn" {
-  name = "/nhs/${var.environment}/deductions_private_alb_httpsl_arn"
-}
+# data "aws_ssm_parameter" "deductions_private_alb_httpsl_arn" {
+#   name = "/nhs/${var.environment}/deductions_private_alb_httpsl_arn"
+# }
 
 data "aws_ssm_parameter" "deductions_private_int_alb_httpl_arn" {
   name = "/nhs/${var.environment}/deductions_private_int_alb_httpl_arn"
@@ -62,4 +62,4 @@ data "aws_ssm_parameter" "deductions_private_int_alb_httpsl_arn" {
 
 data "aws_ssm_parameter" "deductions_private_alb_internal_dns" {
    name = "/nhs/${var.environment}/deductions_private_alb_internal_dns"
-} 
+}

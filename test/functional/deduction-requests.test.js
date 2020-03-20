388322add90f2951dd8d792405a41a724c794b3f
bfisher <fishey2b@gmail.com>
2020-03-20 09:15:58 +0000

    [PRMT-1006] Changed input arguments to axios.post

--- test/functional/deduction-requests.test.js
+++ test/functional/deduction-requests.test.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 import adapter from 'axios/lib/adapters/http';
 
-describe('End to end test of /deduction-requests/:nhsNumber', () => {
+describe('End to end test of /deduction-requests/:nhsNumber',  () => {
     it('should return a 204 from GP2GP Adaptor with a valid NHS number', () => {
         const nhsNumber =
             process.env.NHS_ENVIRONMENT === 'dev' ? "9473480032" : "9442964410";
 
         return expect(
-            axios.post(`${process.env.SERVICE_URL}/deduction-requests/${nhsNumber}`, {
+            axios.post(`${process.env.SERVICE_URL}/deduction-requests/${nhsNumber}`, null, {
                 headers: {
                     Authorization: process.env.AUTHORIZATION_KEYS.split(',')[0]
                 },

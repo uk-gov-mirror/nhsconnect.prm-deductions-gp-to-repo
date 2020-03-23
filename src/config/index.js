6499fa694ff8099e36924deac318a2a12056ea49
Rosie Amphlett <rosie.amphlett@thoughtworks.com>
2020-03-23 14:00:05 +0000

    [PRMT-1007] Updated config and added tests for config logic

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,15 @@
 const portNumber = 3000;
 
-const config = {
+export const initialiseConfig = () => ({
   nodeEnv: process.env.NODE_ENV,
   gp2gpUrl: process.env.GP2GP_URL,
   gp2gpAuthKeys: process.env.GP2GP_AUTHORIZATION_KEYS,
+  repositoryOdsCode: process.env.REPOSITORY_ODS_CODE || 'B86041',
+  repositoryAsid: process.env.REPOSITORY_ASID || '200000001161',
+  practiceOdsCode: 'M85019',
+  practiceAsid: '200000000149',
   url: process.env.SERVICE_URL || `http://127.0.0.1:${portNumber}`
-};
+});
 
-export default config;
+export default initialiseConfig();
 export { portNumber };

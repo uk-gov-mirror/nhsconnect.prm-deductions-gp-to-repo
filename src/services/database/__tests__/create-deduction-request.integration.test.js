1a1a59828fb5108c09ab2a03976002f565f5322a
Rosie Amphlett <rosie.amphlett1@nhs.net>
2020-10-26 18:36:28 +0000

    [PRMT-1289] Refactored createDeductionRequest tests

--- src/services/database/__tests__/create-deduction-request.integration.test.js
+++ src/services/database/__tests__/create-deduction-request.integration.test.js
@@ -1,6 +1,7 @@
 import ModelFactory from '../../../models';
 import { createDeductionRequest } from '../create-deduction-request';
 import { updateLogEvent, updateLogEventWithError } from '../../../middleware/logging';
+import { runWithinTransaction } from '../helper';
 
 jest.mock('../../../middleware/logging');
 
@@ -8,12 +9,12 @@ describe('createDeductionRequest', () => {
   const nhsNumber = '1234567890';
   const conversationId = '099cd501-034f-4e17-a461-cf4fd93ae0cf';
   const odsCode = 'B1234';
-
+  const DeductionRequests = ModelFactory.getByName('DeductionRequests');
   afterAll(() => {
     ModelFactory.sequelize.close();
   });
 
-  it('should call updateLogEvent if data persisted correctly', async () => {
+  it('should call updateLogEvent if data persisted correctly', () => {
     return createDeductionRequest(conversationId, nhsNumber, odsCode).then(() => {
       expect(updateLogEvent).toHaveBeenCalledTimes(1);
       return expect(updateLogEvent).toHaveBeenCalledWith({
@@ -22,15 +23,22 @@ describe('createDeductionRequest', () => {
     });
   });
 
-  it('should create deduction request with correct values', () => {
-    return createDeductionRequest(conversationId, nhsNumber, odsCode).then(request => {
-      expect(request).not.toBeNull();
-      expect(request.get().conversation_id).toBe(conversationId);
-      return expect(request.get().nhs_number).toBe(nhsNumber);
-    });
+  it('should create deduction request with correct values', async () => {
+    await createDeductionRequest(conversationId, nhsNumber, odsCode);
+    const deductionRequest = await runWithinTransaction(transaction =>
+      DeductionRequests.findOne({
+        where: {
+          conversation_id: conversationId
+        },
+        transaction: transaction
+      })
+    );
+    expect(deductionRequest).not.toBeNull();
+    expect(deductionRequest.get().conversation_id).toBe(conversationId);
+    expect(deductionRequest.get().nhs_number).toBe(nhsNumber);
   });
 
-  it('should log errors when nhs number is invalid', async () => {
+  it('should log errors when nhs number is invalid', () => {
     return createDeductionRequest(conversationId, '123', odsCode).catch(error => {
       expect(updateLogEventWithError).toHaveBeenCalledTimes(1);
       expect(updateLogEventWithError).toHaveBeenCalledWith(error);

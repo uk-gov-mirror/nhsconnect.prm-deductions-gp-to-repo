1c2e108caa724545d02e80f5478edf866c099d17
ewrobel-nhs <edyta.wrobel@nhs.net>
2020-10-27 22:49:44 +0000

    [PRMT-1050] Fixed merge conflicts

--- src/services/database/__tests__/create-deduction-request.integration.test.js
+++ src/services/database/__tests__/create-deduction-request.integration.test.js
@@ -2,10 +2,7 @@ import ModelFactory from '../../../models';
 import { createDeductionRequest } from '../create-deduction-request';
 import { updateLogEvent, updateLogEventWithError } from '../../../middleware/logging';
 import { runWithinTransaction } from '../helper';
-<<<<<<< HEAD
 import { v4 as uuid } from 'uuid';
-=======
->>>>>>> 283a2d1a0a1bb95d02a9ef74867096e41738189d
 
 jest.mock('../../../middleware/logging');
 
@@ -18,10 +15,7 @@ describe('createDeductionRequest', () => {
   });
 
   it('should call updateLogEvent if data persisted correctly', () => {
-<<<<<<< HEAD
     const conversationId = uuid();
-=======
->>>>>>> 283a2d1a0a1bb95d02a9ef74867096e41738189d
     return createDeductionRequest(conversationId, nhsNumber, odsCode).then(() => {
       expect(updateLogEvent).toHaveBeenCalledTimes(1);
       return expect(updateLogEvent).toHaveBeenCalledWith({
@@ -31,10 +25,7 @@ describe('createDeductionRequest', () => {
   });
 
   it('should create deduction request with correct values', async () => {
-<<<<<<< HEAD
     const conversationId = uuid();
-=======
->>>>>>> 283a2d1a0a1bb95d02a9ef74867096e41738189d
     await createDeductionRequest(conversationId, nhsNumber, odsCode);
     const deductionRequest = await runWithinTransaction(transaction =>
       DeductionRequests.findOne({
@@ -50,10 +41,7 @@ describe('createDeductionRequest', () => {
   });
 
   it('should log errors when nhs number is invalid', () => {
-<<<<<<< HEAD
     const conversationId = uuid();
-=======
->>>>>>> 283a2d1a0a1bb95d02a9ef74867096e41738189d
     return createDeductionRequest(conversationId, '123', odsCode).catch(error => {
       expect(updateLogEventWithError).toHaveBeenCalledTimes(1);
       expect(updateLogEventWithError).toHaveBeenCalledWith(error);

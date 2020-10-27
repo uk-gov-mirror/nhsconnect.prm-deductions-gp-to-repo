283a2d1a0a1bb95d02a9ef74867096e41738189d
Edyta Wrobel <edyta.wrobel@nhs.net>
2020-10-27 18:04:47 +0000

    [PRMT-1050] Retrieved deduction request by conversationId - wip

--- src/services/__tests__/deduction-request-repository.integration.test.js
+++ src/services/__tests__/deduction-request-repository.integration.test.js
@@ -1,33 +1,36 @@
 import ModelFactory from '../../models';
-import {getDeductionRequestByConversationId} from "../database/deduction-request-repository";
+import { getDeductionRequestByConversationId } from '../database/deduction-request-repository';
 
-describe("Deduction request repository", () => {
+describe('Deduction request repository', () => {
+  const DeductionRequests = ModelFactory.getByName('DeductionRequests');
+  let transaction;
+  ModelFactory.sequelize.transaction().then(t => (transaction = t));
 
-    const DeductionRequests = ModelFactory.getByName('DeductionRequests')
+  const conversationId = '22a748b2-fef6-412d-b93a-4f6c68f0f8dd';
+  afterAll(async () => {
+    // await transaction.rollback();
+    await transaction.rollback();
+    ModelFactory.sequelize.close();
+    // } catch (err) {
 
-    beforeAll(async() => {
-        DeductionRequests.destroy({where: {status: "pds_update_sent"}});
-    })
-    afterAll(() => {
-        DeductionRequests.destroy({where: {status: "pds_update_sent"}});
-        ModelFactory.sequelize.close();
-    });
+  });
+  it('should retrieve DeductionRequest via conversation id', async () => {
+    const expectedNhsNumber = '1234567890';
+    // try {
+    await DeductionRequests.create(
+      {
+        conversation_id: conversationId,
+        nhs_number: expectedNhsNumber,
+        status: 'pds_update_sent',
+        ods_code: 'something'
+      },
+      { transaction }
+    );
+    await transaction.commit();
+    // }
 
-    it("should retrieve DeductionRequest via conversation id", async () => {
-        const expectedNhsNumber = "1234567890";
-        const conversationId = "10297bfb-35de-4fcd-b4e3-3cbf720f8424";
-
-        await DeductionRequests.create(
-            {
-                conversation_id: conversationId,
-                nhs_number: expectedNhsNumber,
-                status: "pds_update_sent",
-                ods_code: "something"
-            }
-        )
-
-        const deductionRequest = await getDeductionRequestByConversationId(conversationId)
-
-        expect(deductionRequest.nhs_number).toBe(expectedNhsNumber)
-    })
-})
+    //   console.log(err);
+    const deductionRequest = await getDeductionRequestByConversationId(conversationId);
+    expect(deductionRequest.nhsNumber).toBe(expectedNhsNumber);
+  });
+});

5e36204660eb086717904c65f50f13e7acfbdd15
Edyta Wrobel <edyta.wrobel@nhs.net>
2020-10-27 18:04:47 +0000

    [PRMT-1050] Retrieved deduction request by conversationId - wip

--- src/services/database/deduction-request-repository.js
+++ src/services/database/deduction-request-repository.js
@@ -1,3 +1,9 @@
-export function getDeductionRequestByConversationId() {
+import ModelFactory from '../../models';
 
-}
+const DeductionRequests = ModelFactory.getByName('DeductionRequests');
+
+export const getDeductionRequestByConversationId = conversationId => {
+  return DeductionRequests.findByPk(conversationId).then(deductionRequest => ({
+    nhsNumber: deductionRequest.nhs_number
+  }));
+};

be932413ebe388a43f477e5bb498f57b1c5b469d
ewrobel-nhs <edyta.wrobel@nhs.net>
2020-10-30 14:35:14 +0000

    [PRMT-1050] Enhanced logging for flaky functional test

--- test/functional/health-record-requests.test.js
+++ test/functional/health-record-requests.test.js
@@ -2,19 +2,24 @@ import axios from 'axios';
 import adapter from 'axios/lib/adapters/http';
 
 describe('POST /health-record-requests/:nhsNumber', () => {
-  it('should test successful POST request for /health-record-requests/:nhsNumber with Authorization', () => {
+  it('should test successful POST request for /health-record-requests/:nhsNumber with Authorization', async () => {
     const nhsNumber = process.env.NHS_ENVIRONMENT === 'dev' ? '9473480032' : '9442964410';
-    return expect(
-      axios.post(`${process.env.SERVICE_URL}/health-record-requests/${nhsNumber}`, null, {
+    const postPromise = axios.post(
+      `${process.env.SERVICE_URL}/health-record-requests/${nhsNumber}`,
+      null,
+      {
         headers: {
           Authorization: process.env.AUTHORIZATION_KEYS
         },
         adapter
-      })
-    ).resolves.toEqual(
-      expect.objectContaining({
-        status: 200
-      })
+      }
     );
+    try {
+      const res = await postPromise;
+      expect(res.status).toBe(200);
+    } catch (err) {
+      console.log(err.toJSON());
+      expect(err).toBe(null);
+    }
   });
 });

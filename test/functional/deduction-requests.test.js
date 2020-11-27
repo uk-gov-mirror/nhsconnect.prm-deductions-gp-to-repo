fbeadb40d6694a344e7f2d23d1cbc0bd09cd8d72
Mircea M <mircea.moise@gmail.com>
2020-11-27 13:31:34 +0000

    [PRMT-1303] Increased time waiting for the http call to come back from a default 5s to 7s We noticed how the http call can take slightly longer than 5s seconds as it goes all the way to PDS and back

--- test/functional/deduction-requests.test.js
+++ test/functional/deduction-requests.test.js
@@ -1,25 +1,30 @@
 import axios from 'axios';
 import adapter from 'axios/lib/adapters/http';
 
+const WAIT_FOR_HTTP_CALL = 7000;
 describe('End to end test of /deduction-requests/', () => {
-  it('should return a 201 from GP2GP Adaptor with a valid NHS number', () => {
-    const nhsNumber = process.env.NHS_ENVIRONMENT === 'dev' ? '9473480032' : '9442964410';
+  it(
+    'should return a 201 from GP2GP Adaptor with a valid NHS number',
+    () => {
+      const nhsNumber = process.env.NHS_ENVIRONMENT === 'dev' ? '9473480032' : '9442964410';
 
-    return expect(
-      axios.post(
-        `${process.env.SERVICE_URL}/deduction-requests/`,
-        { nhsNumber },
-        {
-          headers: {
-            Authorization: process.env.AUTHORIZATION_KEYS
-          },
-          adapter
-        }
-      )
-    ).resolves.toEqual(
-      expect.objectContaining({
-        status: 201
-      })
-    );
-  });
+      return expect(
+        axios.post(
+          `${process.env.SERVICE_URL}/deduction-requests/`,
+          { nhsNumber },
+          {
+            headers: {
+              Authorization: process.env.AUTHORIZATION_KEYS
+            },
+            adapter
+          }
+        )
+      ).resolves.toEqual(
+        expect.objectContaining({
+          status: 201
+        })
+      );
+    },
+    WAIT_FOR_HTTP_CALL
+  );
 });

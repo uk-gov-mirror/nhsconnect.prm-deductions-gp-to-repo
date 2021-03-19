7d2d4a4fb2b97fa7b8fb61030f445b96eeb67d1a
aiwatko <oktawia.kata1@nhs.net>
2021-03-19 14:01:23 +0000

    [PRMT-1556] Added test for structure logging containing conversationId and traceId

--- src/__tests__/app.integration.test.js
+++ src/__tests__/app.integration.test.js
@@ -2,9 +2,11 @@ import app from '../app';
 import axios from 'axios';
 import request from 'supertest';
 import config from '../config';
+import { logger } from '../config/logging';
 import ModelFactory from '../models';
 import { v4 as uuid } from 'uuid';
 import { modelName, Status } from '../models/deduction-request';
+import { transportSpy, expectStructuredLogToContain } from "../__builders__/logging-helper";
 
 jest.mock('axios');
 
@@ -21,6 +23,7 @@ const DeductionRequest = ModelFactory.getByName(modelName);
 
 describe('app', () => {
   beforeEach(() => {
+    logger.add(transportSpy);
     process.env.AUTHORIZATION_KEYS = 'correct-key';
     axios.get.mockImplementation(() => Promise.resolve({ status: 200, data: retrievalResponse }));
     axios.patch.mockImplementation(() => Promise.resolve({ status: 204, data: {} }));
@@ -39,101 +42,99 @@ describe('app', () => {
   });
 
   describe('GET /', () => {
-    it('should return a 404 status code', done => {
-      request(app).get('/').expect(404).end(done);
+    it('should return a 404 status code', async () => {
+      const res = await request(app).get('/');
+
+      expect(res.statusCode).toBe(404);
     });
   });
 
   describe('GET /any-text - an unspecified endpoint', () => {
-    it('should return a 404 status code', done => {
-      request(app).get('/any-text').expect(404).end(done);
+    it('should return a 404 status code', async () => {
+      const res = await request(app).get('/any-text');
+
+      expect(res.statusCode).toBe(404);
     });
   });
 
   describe('Swagger Documentation', () => {
-    it('GET /swagger - should return a 301 status code (redirect) and text/html content type response', done => {
-      request(app)
-        .get('/swagger')
-        .expect(301)
-        .expect('Content-Type', 'text/html; charset=UTF-8')
-        .end(done);
+    it('GET /swagger - should return a 301 status code (redirect) and text/html content type response', async () => {
+      const res = await request(app).get('/swagger')
+
+      expect(res.statusCode).toBe(301);
+      expect(res.header['content-type']).toBe('text/html; charset=UTF-8')
     });
 
-    it('GET /swagger/index.html - should return a 200 status code and text/html content type response', done => {
-      request(app)
-        .get('/swagger/index.html')
-        .expect(200)
-        .expect('Content-Type', 'text/html; charset=UTF-8')
-        .end(done);
+    it('GET /swagger/index.html - should return a 200 status code and text/html content type response', async () => {
+      const res = await request(app).get('/swagger/index.html')
+
+      expect(res.statusCode).toBe(200);
+      expect(res.header['content-type']).toBe('text/html; charset=UTF-8')
     });
   });
 
   describe('GET /health', () => {
-    it('should return a 200 status code', done => {
-      request(app)
-        .get('/health')
-        .expect('Content-Type', 'application/json; charset=utf-8')
-        .expect(200, {
-          version: '1',
-          description: 'Health of GP to Repo service',
-          node_env: config.nodeEnv,
-          details: {
-            database: { type: 'postgresql', connection: true, writable: true }
-          }
-        })
-        .end(done);
+    it('should return a 200 status code', async () => {
+      const res = await request(app).get('/health');
+
+      expect(res.statusCode).toBe(200);
+      expect(res.body).toEqual({
+        version: '1',
+        description: 'Health of GP to Repo service',
+        node_env: config.nodeEnv,
+        details: {
+          database: { type: 'postgresql', connection: true, writable: true }
+        }
+      })
+      expect(res.header['content-type']).toBe('application/json; charset=utf-8')
     });
   });
 
   describe('POST /deduction-requests/', () => {
-    it('should return a 201 status code and empty body for /deduction-requests/', done => {
-      request(app)
+    it('should return a 201 status code and empty body for /deduction-requests/', async () => {
+      const res = await request(app)
         .post('/deduction-requests/')
         .send({ nhsNumber: '1111111111' })
         .set('Authorization', 'correct-key')
-        .expect(201)
-        .expect(res => {
-          expect(res.body).toEqual({});
-        })
-        .end(done);
+
+      expect(res.statusCode).toBe(201);
+      expect(res.body).toEqual({})
+      expectStructuredLogToContain(transportSpy, { conversationId: expect.anything(), traceId: expect.anything() });
     });
 
-    it('should return a location header with lowercase conversationId', done => {
-      request(app)
+    it('should return a location header with lowercase conversationId', async () => {
+      const res = await request(app)
         .post('/deduction-requests/')
         .send({ nhsNumber: '1111111111' })
         .set('Authorization', 'correct-key')
-        .expect(201)
-        .expect(res => {
-          expect(res.header.location).toMatch(
-            /^.*([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[89ab][0-9a-f]{3}-[0-9a-f]{12}).*$/
-          );
-        })
-        .end(done);
+
+      expect(res.statusCode).toBe(201);
+      expect(res.header.location).toMatch(/^.*([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[89ab][0-9a-f]{3}-[0-9a-f]{12}).*$/)
+      expectStructuredLogToContain(transportSpy, { conversationId: expect.anything(), traceId: expect.anything() });
     });
 
-    it('should return a 503 status code with Errors for /deduction-requests/', done => {
+    it('should return a 503 status code with Errors for /deduction-requests/', async () => {
       axios.patch.mockImplementation(() =>
         Promise.resolve({ status: 200, data: 'incorrect data!' })
       );
 
-      request(app)
+      const res = await request(app)
         .post('/deduction-requests/')
         .send({ nhsNumber: '9999999999' })
         .set('Authorization', 'correct-key')
-        .expect(503)
-        .expect(res => {
-          expect(res.body).toEqual({ errors: 'Failed to Update: incorrect data!' });
-        })
-        .end(done);
+
+      expect(res.statusCode).toBe(503);
+      expect(res.body).toEqual({ errors: 'Failed to Update: incorrect data!' })
+      expectStructuredLogToContain(transportSpy, { conversationId: expect.anything(), traceId: expect.anything() });
     });
 
-    it('should return a 422 status code without nhsNumber in body', done => {
-      request(app)
+    it('should return a 422 status code without nhsNumber in body', async () => {
+      const res = await request(app)
         .post('/deduction-requests/')
         .set('Authorization', 'correct-key')
-        .expect(422)
-        .end(done);
+
+      expect(res.statusCode).toBe(422);
+      expectStructuredLogToContain(transportSpy, { traceId: expect.anything() });
     });
   });
 
@@ -142,7 +143,7 @@ describe('app', () => {
     const expectedNhsNumber = '1234567890';
     const expectedStatus = Status.PDS_UPDATE_SENT;
 
-    it('should return deduction request info', async done => {
+    it('should return deduction request info', async () => {
       await DeductionRequest.create({
         conversationId,
         nhsNumber: expectedNhsNumber,
@@ -150,32 +151,32 @@ describe('app', () => {
         odsCode: 'something'
       });
 
-      request(app)
+      const res = await request(app)
         .get(`/deduction-requests/${conversationId}`)
         .set('Authorization', 'correct-key')
-        .expect(200)
-        .expect(res => {
-          expect(res.body).toEqual({
-            data: {
-              type: 'deduction-requests',
-              id: conversationId,
-              attributes: {
-                nhsNumber: expectedNhsNumber,
-                status: expectedStatus
-              }
-            }
-          });
-        })
-        .end(done);
+
+      expect(res.statusCode).toBe(200);
+      expect(res.body).toEqual({
+        data: {
+          type: 'deduction-requests',
+          id: conversationId,
+          attributes: {
+            nhsNumber: expectedNhsNumber,
+            status: expectedStatus
+          }
+        }
+      })
+      expectStructuredLogToContain(transportSpy, { conversationId, traceId: expect.anything() });
     });
 
-    it('should return 404 when deduction request cannot be found', done => {
-      const invalidConversationId = '58eff803-48f3-4a30-8189-632141bd823d';
-      request(app)
-        .get(`/deduction-requests/${invalidConversationId}`)
+    it('should return 404 when deduction request cannot be found', async () => {
+      const nonExistingConversationId = '58eff803-48f3-4a30-8189-632141bd823d';
+      const res = await request(app)
+        .get(`/deduction-requests/${nonExistingConversationId}`)
         .set('Authorization', 'correct-key')
-        .expect(404)
-        .end(done);
+
+      expect(res.statusCode).toBe(404);
+      expectStructuredLogToContain(transportSpy, { conversationId: nonExistingConversationId, traceId: expect.anything() });
     });
   });
 
@@ -185,7 +186,7 @@ describe('app', () => {
     const expectedStatus = Status.PDS_UPDATE_SENT;
     const odsCode = 'B1234';
 
-    it('should return 204 upon successful deduction request PDS update', async done => {
+    it('should return 204 upon successful deduction request PDS update', async () => {
       await DeductionRequest.create({
         conversationId,
         nhsNumber: expectedNhsNumber,
@@ -193,14 +194,13 @@ describe('app', () => {
         odsCode
       });
 
-      request(app)
+      const res = await request(app)
         .patch(`/deduction-requests/${conversationId}/pds-updated`)
         .set('Authorization', 'correct-key')
-        .expect(204)
-        .expect(res => {
-          expect(res.body).toEqual({});
-        })
-        .end(done);
+
+      expect(res.statusCode).toBe(204);
+      expect(res.body).toEqual({})
+      expectStructuredLogToContain(transportSpy, { conversationId, traceId: expect.anything() });
     });
   });
 
@@ -211,7 +211,7 @@ describe('app', () => {
     const status = Status.EHR_REQUEST_SENT;
     const odsCode = 'B1234';
 
-    it('should return 204 when EHR message is received', async done => {
+    it('should return 204 when EHR message is received', async () => {
       axios.get.mockImplementation(() => Promise.resolve({ status: 200 }));
       await DeductionRequest.create({
         conversationId,
@@ -220,15 +220,14 @@ describe('app', () => {
         odsCode
       });
 
-      request(app)
+      const res = await  request(app)
         .patch(`/deduction-requests/${conversationId}/ehr-message-received`)
         .send({ messageId })
         .set('Authorization', 'correct-key')
-        .expect(204)
-        .expect(res => {
-          expect(res.body).toEqual({});
-        })
-        .end(done);
+
+      expect(res.statusCode).toBe(204);
+      expect(res.body).toEqual({})
+      expectStructuredLogToContain(transportSpy, { messageId, conversationId, traceId: expect.anything() });
     });
   });
 
@@ -269,6 +268,7 @@ describe('app', () => {
           }
         }
       });
+      expectStructuredLogToContain(transportSpy, { conversationId, traceId: expect.anything() });
     });
   });
 });

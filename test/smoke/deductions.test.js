229bb57c85e7b99b3a88a2fb379fd13a8ebbefa0
aiwatko <oktawia.kata1@nhs.net>
2021-04-14 18:33:54 +0100

    [PRMT-1527] Refactored e2e test to pool instead of sleeping

--- test/smoke/deductions.test.js
+++ test/smoke/deductions.test.js
@@ -2,238 +2,259 @@ import axios from 'axios';
 import adapter from 'axios/lib/adapters/http';
 import { v4 as uuid } from 'uuid';
 
-const assignPatientToTPPOdsCode = async (nhsNumber, tppOdsCode) => {
-  // - get the PDS info
-  const pdsResponse = await axios
-    .get(`${process.env.GP2GP_URL}/patient-demographics/${nhsNumber}/`, {
-      headers: {
-        Authorization: process.env.GP2GP_AUTHORIZATION_KEYS
-      },
-      adapter
-    })
-    .catch(err => {
-      console.log(err);
-    });
-
-  // - update PDS
-  const patchResponse = await axios
-    .patch(
-      `${process.env.GP2GP_URL}/patient-demographics/${nhsNumber}`,
-      {
-        pdsId: pdsResponse.data.data.patientPdsId,
-        serialChangeNumber: pdsResponse.data.data.serialChangeNumber,
-        newOdsCode: tppOdsCode,
-        conversationId: uuid()
-      },
-      {
-        headers: {
-          Authorization: process.env.GP2GP_AUTHORIZATION_KEYS
-        },
-        adapter
-      }
-    )
-    .catch(err => {
-      console.log(err);
-    });
-
-  //we have to wait as PDS needs time to process the update
-  //to investigate: when there is no sleep, the get fails with 503, we need find out why
-  await sleep(500);
-
-  expect(patchResponse.status).toBe(204);
-  const pdsResponseCheck = await axios
-    .get(`${process.env.GP2GP_URL}/patient-demographics/${nhsNumber}/`, {
-      headers: {
-        Authorization: process.env.GP2GP_AUTHORIZATION_KEYS
-      },
-      adapter
-    })
-    .catch(err => {
-      console.log(err);
-    });
-  expect(pdsResponseCheck.data.data.odsCode).toBe(tppOdsCode);
-};
-
-const assignPatientToEMISOdsCode = async (nhsNumber, emisOdsCode) => {
-  // - get the PDS info
-  const pdsResponse = await axios
-    .get(`${process.env.GP2GP_URL}/patient-demographics/${nhsNumber}/`, {
-      headers: {
-        Authorization: process.env.GP2GP_AUTHORIZATION_KEYS
-      },
-      adapter
-    })
-    .catch(err => {
-      console.log(err);
-    });
-
-  // - update PDS
-  const patchResponse = await axios
-    .patch(
-      `${process.env.GP2GP_URL}/patient-demographics/${nhsNumber}`,
-      {
-        pdsId: pdsResponse.data.data.patientPdsId,
-        serialChangeNumber: pdsResponse.data.data.serialChangeNumber,
-        newOdsCode: emisOdsCode,
-        conversationId: uuid()
-      },
-      {
-        headers: {
-          Authorization: process.env.GP2GP_AUTHORIZATION_KEYS
-        },
-        adapter
-      }
-    )
-    .catch(err => {
-      console.log(err);
-    });
-
-  //we have to wait as PDS needs time to process the update
-  //to investigate: when there is no sleep, the get fails with 503, we need find out why
-  await sleep(500);
-
-  expect(patchResponse.status).toBe(204);
-  const pdsResponseCheck = await axios
-    .get(`${process.env.GP2GP_URL}/patient-demographics/${nhsNumber}/`, {
-      headers: {
-        Authorization: process.env.GP2GP_AUTHORIZATION_KEYS
-      },
-      adapter
-    })
-    .catch(err => {
-      console.log(err);
-    });
-  expect(pdsResponseCheck.data.data.odsCode).toBe(emisOdsCode);
-};
-
 describe('Smoke test for deduction process', () => {
-  it('should handle a deduction for a patient of TPP practice with the small Health Record', async () => {
-    const nhsNumber = '9442964410';
-    const tppOdsCode = 'M85019';
+  const RETRY_COUNT = 30;
+  const POLLING_INTERVAL_MS = 500;
+  const TEST_TIMEOUT = 3 * RETRY_COUNT * POLLING_INTERVAL_MS;
 
-    console.log('GP2GP Adaptor URL:', process.env.GP2GP_URL);
-    //setup: make sure the patient nhs number is assigned to TPP in PDS
-    await assignPatientToTPPOdsCode(nhsNumber, tppOdsCode);
-    //action: call /deduction-request with the TPP patient number
+  it(
+    'should handle a deduction for a patient of TPP practice with the small Health Record',
+    async () => {
+      const nhsNumber = '9442964410';
+      const tppOdsCode = 'M85019';
 
-    const response = await axios
-      .post(
-        `${process.env.SERVICE_URL}/deduction-requests/`,
-        { nhsNumber },
-        {
+      // Setup: make sure the patient nhs number is assigned to EMIS in PDS
+      await assignPatientToOdsCode(nhsNumber, tppOdsCode);
+
+      // PDS update polling
+      let patientOdsCode;
+      for (let i = 0; i < RETRY_COUNT; i++) {
+        patientOdsCode = await getPatientOdsCode(nhsNumber);
+        console.log(`try: ${i} - status: ${patientOdsCode}`);
+
+        if (patientOdsCode === tppOdsCode) {
+          break;
+        }
+        await sleep(POLLING_INTERVAL_MS);
+      }
+
+      expect(patientOdsCode).toEqual(tppOdsCode);
+
+      // Action: call /deduction-request with the TPP patient number
+      const deductionRequestResponse = await axios
+        .post(
+          `${process.env.SERVICE_URL}/deduction-requests/`,
+          { nhsNumber },
+          {
+            headers: {
+              Authorization: process.env.AUTHORIZATION_KEYS
+            },
+            adapter
+          }
+        )
+        .catch(err => {
+          console.log(err);
+        });
+
+      const deductionRequestResourceUrl = deductionRequestResponse.headers['location'];
+
+      // Assertion
+      // 1) status of the deduction is "ehr-extract-received"
+      // 2) ehr-repo has the EHR (??)
+
+      // Deduction request status update polling
+      const expectedStatus = 'ehr_extract_received';
+      let deductionRequestStatus;
+      for (let i = 0; i < RETRY_COUNT; i++) {
+        deductionRequestStatus = await getDeductionRequestStatus(
+          nhsNumber,
+          deductionRequestResourceUrl
+        );
+        console.log(`try: ${i} - status: ${deductionRequestStatus}`);
+
+        if (deductionRequestStatus === expectedStatus) {
+          break;
+        }
+        await sleep(POLLING_INTERVAL_MS);
+      }
+      expect(deductionRequestStatus).toBe(expectedStatus);
+    },
+    TEST_TIMEOUT
+  );
+
+  it(
+    'should handle a deduction for a patient of EMIS practice with the large Health Record',
+    async () => {
+      const nhsNumber = '9692295427';
+      const emisOdsCode = 'N82668';
+
+      // Setup: make sure the patient nhs number is assigned to EMIS in PDS
+      await assignPatientToOdsCode(nhsNumber, emisOdsCode);
+
+      // PDS update polling
+      let patientOdsCode;
+      for (let i = 0; i < RETRY_COUNT; i++) {
+        patientOdsCode = await getPatientOdsCode(nhsNumber);
+        console.log(`try: ${i} - status: ${patientOdsCode}`);
+
+        if (patientOdsCode === emisOdsCode) {
+          break;
+        }
+        await sleep(POLLING_INTERVAL_MS);
+      }
+
+      expect(patientOdsCode).toEqual(emisOdsCode);
+
+      // Action: call /deduction-request with the EMIS patient number
+      const response = await axios
+        .post(
+          `${process.env.SERVICE_URL}/deduction-requests/`,
+          { nhsNumber },
+          {
+            headers: {
+              Authorization: process.env.AUTHORIZATION_KEYS
+            },
+            adapter
+          }
+        )
+        .catch(err => {
+          console.log(err.response);
+        });
+
+      const deductionRequestResourceUrl = response.headers['location'];
+      const splitDeductionRequestResourceUrl = response.headers['location'].split('/');
+      const conversationId =
+        splitDeductionRequestResourceUrl[splitDeductionRequestResourceUrl.length - 1];
+      console.log('DeductionRequest conversationId:', conversationId);
+
+      // Deduction request status update polling
+      const expectedStatus = 'continue_message_sent';
+      let deductionRequestStatus;
+      for (let i = 0; i < RETRY_COUNT; i++) {
+        deductionRequestStatus = await getDeductionRequestStatus(
+          nhsNumber,
+          deductionRequestResourceUrl
+        );
+        console.log(`try: ${i} - status: ${deductionRequestStatus}`);
+
+        if (deductionRequestStatus === expectedStatus) {
+          break;
+        }
+        await sleep(POLLING_INTERVAL_MS);
+      }
+      expect(deductionRequestStatus).toBe(expectedStatus);
+
+      // Assertion: ehr-repo has the large EHR for the specific transfer
+
+      // Health record status polling
+      let patientHealthRecordStatus;
+      for (let i = 0; i < RETRY_COUNT; i++) {
+        patientHealthRecordStatus = await getHealthRecordStatus(nhsNumber, conversationId);
+        console.log(`try: ${i} - status: ${patientHealthRecordStatus}`);
+
+        if (patientHealthRecordStatus === 200) {
+          break;
+        }
+        await sleep(POLLING_INTERVAL_MS);
+      }
+      expect(patientHealthRecordStatus).toBe(200);
+
+      const patientHealthRecord = await axios
+        .get(`${process.env.EHR_REPO_URL}/patients/${nhsNumber}`, {
           headers: {
-            Authorization: process.env.AUTHORIZATION_KEYS
+            Authorization: process.env.EHR_REPO_AUTHORIZATION_KEYS
           },
           adapter
-        }
-      )
-      .catch(err => {
-        console.log(err);
-      });
-    const deductionRequestResourceUrl = response.headers['location'];
+        })
+        .catch(err => {
+          console.log(err.response);
+        });
 
-    //assertion: 1) status of the deduction is "ehr-extract-received"; 2) ehr-repo has the EHR (??)
-
-    await sleep(10000);
-    const deductionRequestResource = await axios
-      .get(deductionRequestResourceUrl, {
-        headers: {
-          Authorization: process.env.AUTHORIZATION_KEYS
-        },
-        adapter
-      })
-      .catch(err => {
-        console.log(err.response);
-      });
-
-    expect(deductionRequestResource.status).toBe(200);
-    const responseBody = deductionRequestResource.data;
-    expect(responseBody.data.attributes.status).toBe('ehr_extract_received');
-  }, 30000);
-
-  it('should handle a deduction for a patient of EMIS practice with the large Health Record', async () => {
-    const nhsNumber = '9692295427';
-    const emisOdsCode = 'N82668';
-
-    console.log('GP2GP Adaptor URL:', process.env.GP2GP_URL);
-    //setup: make sure the patient nhs number is assigned to EMIS in PDS
-    await assignPatientToEMISOdsCode(nhsNumber, emisOdsCode);
-    //action: call /deduction-request with the EMIS patient number
-
-    const response = await axios
-      .post(
-        `${process.env.SERVICE_URL}/deduction-requests/`,
-        { nhsNumber },
-        {
-          headers: {
-            Authorization: process.env.AUTHORIZATION_KEYS
-          },
-          adapter
-        }
-      )
-      .catch(err => {
-        console.log(err.response);
-      });
-
-    const deductionRequestResourceUrl = response.headers['location'];
-    const splitDeductionRequestResourceUrl = response.headers['location'].split('/');
-    const conversationId =
-      splitDeductionRequestResourceUrl[splitDeductionRequestResourceUrl.length - 1];
-    console.log('DeductionRequest conversationId:', conversationId);
-
-    await sleep(10000);
-    const deductionRequestResource = await axios
-      .get(deductionRequestResourceUrl, {
-        headers: {
-          Authorization: process.env.AUTHORIZATION_KEYS
-        },
-        adapter
-      })
-      .catch(err => {
-        console.log(err.response);
-      });
-
-    expect(deductionRequestResource.status).toBe(200);
-    const responseBody = deductionRequestResource.data;
-    expect(responseBody.data.attributes.status).toBe('continue_message_sent');
-
-    //assertion: 2) ehr-repo has the large EHR for the specific transfer
-    const ehrRepoUrl = process.env.EHR_REPO_URL;
-    console.log('EHR Repo URL:', process.env.EHR_REPO_URL);
-
-    await sleep(10000);
-    const healthRecordResourceStatus = await axios
-      .get(`${ehrRepoUrl}/patients/${nhsNumber}/health-records/${conversationId}`, {
-        headers: {
-          Authorization: process.env.EHR_REPO_AUTHORIZATION_KEYS
-        },
-        adapter
-      })
-      .catch(err => {
-        console.log(err.response);
-      });
-
-    expect(healthRecordResourceStatus.status).toBe(200);
-
-    const healthRecordResource = await axios
-      .get(`${ehrRepoUrl}/patients/${nhsNumber}`, {
-        headers: {
-          Authorization: process.env.EHR_REPO_AUTHORIZATION_KEYS
-        },
-        adapter
-      })
-      .catch(err => {
-        console.log(err.response);
-      });
-
-    expect(healthRecordResource.status).toBe(200);
-    expect(healthRecordResource.data.data.links['attachments']).toHaveLength(2);
-    expect(healthRecordResource.data.data.links['attachments'][0]).toContain(conversationId);
-  }, 30000);
+      expect(patientHealthRecord.status).toBe(200);
+      expect(patientHealthRecord.data.data.links['attachments']).toHaveLength(2);
+      expect(patientHealthRecord.data.data.links['attachments'][0]).toContain(conversationId);
+    },
+    TEST_TIMEOUT
+  );
 });
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+const assignPatientToOdsCode = async (nhsNumber, odsCode) => {
+  // - get the PDS info
+  const pdsResponse = await axios
+    .get(`${process.env.GP2GP_URL}/patient-demographics/${nhsNumber}/`, {
+      headers: {
+        Authorization: process.env.GP2GP_AUTHORIZATION_KEYS
+      },
+      adapter
+    })
+    .catch(err => {
+      console.log(err);
+    });
 
-// call the other endpoint
+  // - update PDS
+  const patchResponse = await axios
+    .patch(
+      `${process.env.GP2GP_URL}/patient-demographics/${nhsNumber}`,
+      {
+        pdsId: pdsResponse.data.data.patientPdsId,
+        serialChangeNumber: pdsResponse.data.data.serialChangeNumber,
+        newOdsCode: odsCode,
+        conversationId: uuid()
+      },
+      {
+        headers: {
+          Authorization: process.env.GP2GP_AUTHORIZATION_KEYS
+        },
+        adapter
+      }
+    )
+    .catch(err => {
+      console.log(err);
+    });
+
+  expect(patchResponse.status).toBe(204);
+};
+
+const getPatientOdsCode = async nhsNumber => {
+  try {
+    const patientDemographics = await axios.get(
+      `${process.env.GP2GP_URL}/patient-demographics/${nhsNumber}`,
+      {
+        headers: { Authorization: process.env.GP2GP_AUTHORIZATION_KEYS },
+        adapter
+      }
+    );
+    return patientDemographics.data.data.odsCode;
+  } catch (err) {
+    console.log(err.response.status);
+    return undefined;
+  }
+};
+
+const getHealthRecordStatus = async (nhsNumber, conversationId) => {
+  try {
+    const patientHealthRecordResponse = await axios.get(
+      `${process.env.EHR_REPO_URL}/patients/${nhsNumber}/health-records/${conversationId}`,
+      {
+        headers: {
+          Authorization: process.env.EHR_REPO_AUTHORIZATION_KEYS
+        },
+        adapter
+      }
+    );
+    return patientHealthRecordResponse.status;
+  } catch (err) {
+    console.log(err.response.status);
+    return undefined;
+  }
+};
+
+const getDeductionRequestStatus = async (nhsNumber, deductionRequestResourceUrl) => {
+  try {
+    const deductionRequestResponse = await axios.get(deductionRequestResourceUrl, {
+      headers: {
+        Authorization: process.env.AUTHORIZATION_KEYS
+      },
+      adapter
+    });
+
+    return deductionRequestResponse.data.data.attributes.status;
+  } catch (err) {
+    console.log(err.response.status);
+    return undefined;
+  }
+};
+
+const sleep = ms => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
